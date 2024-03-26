9ef2e19edf96c3dd7d7fbc1d207a40dbf711768e
Johnny Zeng <140343760+noobiecoderjohn@users.noreply.github.com>
2024-03-25 23:01:14 -0400

    Update football_stats.py

--- football_stats.py
+++ football_stats.py
@@ -2,27 +2,17 @@
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+url = "https://en.wikipedia.org/wiki/List_of_NBA_champions"
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    tables = soup.find_all("table", class_="wikitable")
 
-def nba_champions():
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find("table", class_="wikitable")
-    nba_list = []
-    headers = [header.text.strip() for header in table.find_all('th')]
-    rows = table.find_all('tr')
-    
-    for row in rows:
-        columns = row.find_all('td')
-        if columns:
-            columns = [column.text.strip() for column in columns]
-            nba_list.append(columns)
-
-    df = pd.DataFrame(nba_list, columns=headers)
-    df = df.replace(r'\[\d+\]', '', regex=True)
-    
-    return df
-
-if __name__ == "__main__":
-    URL = 'https://en.wikipedia.org/wiki/List_of_NBA_champions'
-    df_nba = nba_champions(URL)
-    print(df_nba.head())
+    for i, table in enumerate(tables):
+        df = pd.read_html(str(table))[0]  # Use [0] to get the first table        
+        print(f"Table {i + 1}:")
+        print(df)
+        print("\n")
+else:
+    print("Failed to retrieve the page.")
